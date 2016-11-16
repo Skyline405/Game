fa8e12beb2b587b1b67bfd0dae5cfe5b20924ebf
Skyline <ars.sky@mail.ru>
2016-11-16 23:01:45 +0700

    Добавлена простая анимация спрайта

--- src/index.js
+++ src/index.js
@@ -8,29 +8,52 @@ export class App {
 	constructor(){
 		this.game = new Game({
 			target: '#game',
-			size: [650, 650]
+			size: [480, 640]
 		});
 		this.scene = this.game.getScene();
 
-		let image = new Image();
-		image.src = 'resources/sprites/sprites.png';
+		this.createBG();
+		this.createBird();
 
-		let rect = new Sprite({
-			image: image,
+		this.game.start();
+	}
+
+	createBG(){
+		let backgroundImg = new Image();
+		backgroundImg.src = 'resources/sprites/background.png';
+
+		let background = new Sprite({
+			image: backgroundImg,
+			position: [0, 0],
+			size: [144, 256],
+			offset: [0, 0],
+			scale: 2
+		});
+		this.scene.addObject(background);
+	}
+
+	createBird(){
+		let birdImg = new Image();
+		birdImg.src = 'resources/sprites/bird.png';
+
+		let bird = new Sprite({
+			image: birdImg,
 			position: [50, 50],
 			size: [17, 12],
-			offset: [264, 64],
-			scale: [2],
+			offset: [0, 0],
+			scale: 2,
+			animation: {
+				frames: 3,
+				duration: 5
+			},
 			update(delta){
-				this.position.x += 30 * delta;
-				if(this.position.x > 650){
+				this.position.x += 50 * delta;
+				if(this.position.x > 480){
 					this.position.x = -17;
 				}
 			}
 		});
-		this.scene.addObject(rect);
-
-		this.game.start();
+		this.scene.addObject(bird);
 	}
 
 }
--- src/objects/Sprite.js
+++ src/objects/Sprite.js
@@ -1,34 +1,53 @@
 
 import {GraphicObject} from './GraphicObject';
 import {Vector2} from '../Vector2';
+import {Animation} from './Animation';
 
 
 export class Sprite extends GraphicObject {
 
+	frameIndex = 0;
+	ticksCount = 0;
+
 	constructor(options){
 		super(options);
-		var { image, offset = [0,0], scale = [1,1], animation,  } = options;
+		var { image, offset = [0,0], scale = 1, animation = {frames: 1, duration: 0}  } = options;
 
 		this.image = image;
 		this.offset = Vector2.fromArray(offset);
 		this.scale = scale;
 
-		let { frames = 1, speed } = animation;
-		this.animation = {
-			frames: frames,
-			speed: speed
-		};
+		this.animation = animation;
 
+	}
+
+	update(deltaTime){
+		super.update(deltaTime);
+
+		this.ticksCount++;
+		if (this.ticksCount > this.animation.duration) {
+			this.ticksCount = 0;
+			this.frameIndex++;
+			if (this.frameIndex >= this.animation.frames) {
+				this.frameIndex = 0;
+			}
+		}
 	}
 
 	draw(renderer){
 		let ctx = renderer.ctx;
 		let [x,y] = this.position.toArray();
-		let [dx,dy] = this.offset.toArray();
 		let [w,h] = this.size.toArray();
-		let [sw,sh] = this.scale;
-		sh = sh || sw;
-		ctx.drawImage(this.image, dx,dy, w,h, x,y, w*sw,h*sh);
+
+		let [dx,dy] = this.offset.toArray();
+		let offset = dx + this.frameIndex * w;
+
+		ctx.drawImage(this.image,
+				offset, dy, // отступ от края спрайта
+				w, h, // размеры спрайта
+				x, y, // координаты на холсте
+				w*this.scale, h*this.scale // масштабироание (растягивание)
+		);
 	}
 
 }
